d2765ba1f112bf047d4e36e4a0471cffa3ba3822
Carlos Forero <ecarlosforero@gmail.com>
2017-07-22 02:58:14 -0500

    Add test: TestProduceTwoFactories

--- produce_test.go
+++ produce_test.go
@@ -15,3 +15,14 @@ func TestProduce(t *testing.T) {
 
 	assert.NoError(t, Factory("modelA").Produce(modelA), "Should be produced")
 }
+
+func TestProduceTwoFactories(t *testing.T) {
+	Clear()
+	Factory("modelA").Design(&ModelA{})
+	Factory("modelB").Design(&ModelB{})
+
+	modelA := &ModelA{}
+	modelB := &ModelB{}
+	assert.NoError(t, Factory("modelA").Produce(modelA), "Should be produced")
+	assert.NoError(t, Factory("modelB").Produce(modelB), "Should be produced")
+}
